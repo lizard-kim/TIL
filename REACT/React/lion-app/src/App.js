d2422ecb7d480dd4a1140456fbfe981e8b3ddbb2
taeyeong <rlaxodud1122@gmail.com>
2019-12-29 17:42:10 +0900

    State study

--- REACT/React/lion-app/src/App.js
+++ REACT/React/lion-app/src/App.js
@@ -20,24 +20,18 @@ function App() {
     ["Beijing", 9],
     ["Sydney", 12],
     ["LA", 17],
-    ["Busan", 10]
+    ["Busan", 10],
   ]
-
   const WorldClockList = cityTimeData.map((citytime, index)=>
     <WorldClock city={citytime[0]} time = {citytime[1]} key = {index}/>
   )
-
   return (
     <div className="App">
-      <h1 className={'myStyle'}> Welcome! </h1> 
-      <div className={'post'}>
+      <h1 className={"myStyle"}> Welcome! </h1> 
+      <div className={"post"}>
         this is first posting!
       </div>
         {WorldClockList}
-      <table>
-        <tr><td>1</td></tr>
-        <tr><td>2</td></tr>
-      </table>
     </div>
   );
 }
@@ -55,7 +49,7 @@ function App() {
 } */
 
 //12.29
-function WorldClock(props) {
+function WorldClock2(props) {// props are citytime and index
   return(//div는 한개씩만 쓸 수 있다. div안에 div추가 불가
     <div className = {"WorldClock"}>
       <h2> City: {props.city} </h2>
@@ -64,6 +58,34 @@ function WorldClock(props) {
   )
 }
 
+class WorldClock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hour: this.props.time,
+      minute: 0
+    }
+  
+    setInterval(()=>{
+      this.setState((state)=>(
+        state.minute === 59
+        ?{hour: state.hour + 1, minute: 0}
+        :{minute: state.minute + 1}
+      ))
+    },1000)
+
+  }
+  render() {
+    return(//div는 한개씩만 쓸 수 있다. div안에 div추가 불가
+      <div className = {"WorldClock"}>
+        <h2> City: {this.props.city} </h2>
+        <p> Time: {this.state.hour}:{this.state.minute} </p>
+      </div>
+    )
+  }
+
+}
+                           
 
 // component export
 // default : this file export only one component (defalut) 
