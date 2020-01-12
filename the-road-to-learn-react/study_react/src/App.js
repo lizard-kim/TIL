3ea13fb9e561f5410830a5377c5975a1603434f3
taeyeong <rlaxodud1122@gmail.com>
2020-01-12 19:39:34 +0900

    리액트 기초다지기

--- the-road-to-learn-react/study_react/src/App.js
+++ the-road-to-learn-react/study_react/src/App.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import'./App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-	  	<p>testing</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends React.Component{
+  render(){
+    const comment = "Hello world!" //no change
+    let comment2= "Hello world 2" //changeable
+    return(
+      <div className="App">
+        <h1> {comment} </h1>
+        <h2> {comment2} </h2>
+        {list.map((item)=>{
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
