b3e6a995b4b529f236b26580775cd35e9f72c06a
lizard <rlaxodud1122@gmail.com>
2020-02-25 20:42:05 +0900

    for save

--- Algorithm/baekjoon/1181.cpp
+++ Algorithm/baekjoon/1181.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-void sorting(vector<string>& v){
+class Input{
+	public:
+		std::string input;
+		int len;
+};
 
+void sorting(Input** arr1, int low, int high, Input** arr2){
+	//length == 1
+	if(low >= high) return;
+
+	int mid = (low+high)/2;
+
+	sorting(arr1, low, mid, arr2);
+	sorting(arr1, mid+1, high, arr2);
+
+	//sorting
+	
+	for(int i=low; i < high, i++){
+		if(arr1[i]->len < arr1[i+1]->len){
+			arr2[i] = arr1[i];
+		}
+		else if(arr1[i]->len == arr1[i+1]->len){
+			if(strcmp(arr1[i]->input, arr1[i]->input) >= 0){
+				arr2[i] = arr1[i];
+			}
+			else{
+				arr2[i] = arr1[i+1];
+			}
+		}
+	}
+	
 }
-
 
 int main(){
 	int input = 0;
-	string istr;
+	std::string istr;
 
 	cin >> input;
-
-	vector<string> v(input,"0");
 	
+	Input** Iarr = new Input* [input];
+
 	for(int i=0; i < input; i++){
 		cin >> istr;
-		v.push_back(istr);
+		Iarr[i] = new Input();
+		Iarr[i]->input = istr;
+		Iarr[i]->len = istr.length();
 	}
-
-	sorting(v);
-
-	cout << v.at(2) << endl;
+	
 	
 
+
+	bool d = strcmp("aa", "aa");
+	cout << d << endl;
 }
