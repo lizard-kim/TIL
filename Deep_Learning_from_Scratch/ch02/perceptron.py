0e824dede62dba09446f0357431b3509df7fe8c6
lizard-kim <rlaxodud1122@gmail.com>
2020-10-27 04:06:35 +0900

    Study ch02 finished

--- Deep_Learning_from_Scratch/ch02/perceptron.py
+++ Deep_Learning_from_Scratch/ch02/perceptron.py
@@ -7,10 +7,13 @@ def AND(x1, x2):
     temp = np.sum(w*x) + b
     if temp <= 0:
         return 0
-    elif temp > 1:
+    elif temp > 0:
         return 1
 
+print(AND(0, 0))
 print(AND(0, 1))
+print(AND(1, 0))
+print(AND(1, 1))
 
 def NAND(x1, x2):
     x = np.array([x1, x2])
@@ -21,18 +24,32 @@ def NAND(x1, x2):
         return 0
     elif temp > 0:
         return 1
+print(NAND(0, 0))
 print(NAND(0, 1))
+print(NAND(1, 0))
+print(NAND(1, 1))
 
 def OR(x1, x2):
     x = np.array([x1, x2])
     w = np.array([1, 1])
-    b = 1
+    b = 0
     temp = np.sum(w*x) + b
     if temp <= 0:
         return 0
     elif temp > 0:
         return 1
-print(OR(0, 1))
 print(OR(0, 0))
+print(OR(0, 1))
+print(OR(1, 0))
 print(OR(1, 1))
 
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print(XOR(0,0))
+print(XOR(0,1))
+print(XOR(1,0))
+print(XOR(1,1))
