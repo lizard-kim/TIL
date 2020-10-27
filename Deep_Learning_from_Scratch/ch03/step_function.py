dfa17c4dbb9eb7c9ebd784587a8b4c102d1214c6
lizard-kim <rlaxodud1122@gmail.com>
2020-10-28 03:37:05 +0900

    Study ch03 half

---
+++ Deep_Learning_from_Scratch/ch03/step_function.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+# not good
+# this function get only int
+def bad_step_function(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+
+def good_step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # range of axis y
+plt.show()
