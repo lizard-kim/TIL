9fee34c33b42062260cab7efcf9d03fa742ef185
lizard-kim <rlaxodud1122@gmail.com>
2020-10-30 23:44:25 +0900

    Study ch03 3.4

--- Deep_Learning_from_Scratch/ch03/sigmoid_function.py
+++ Deep_Learning_from_Scratch/ch03/sigmoid_function.py
@@ -6,9 +6,9 @@ def sigmoid(x):
 
 print(sigmoid(np.array([-1.0, 1.0, 2.0])))
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+#  x = np.arange(-5.0, 5.0, 0.1)
+#  y = sigmoid(x)
+#  plt.plot(x, y)
+#  plt.ylim(-0.1, 1.1)
+#  plt.show()
 
