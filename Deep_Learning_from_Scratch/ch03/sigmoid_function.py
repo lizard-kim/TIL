1de4b7b1a698e9827d05c5713eb4d64a7b46ba4b
lizard-kim <rlaxodud1122@gmail.com>
2020-10-31 17:12:38 +0900

    Study ch03 finished

--- Deep_Learning_from_Scratch/ch03/sigmoid_function.py
+++ Deep_Learning_from_Scratch/ch03/sigmoid_function.py
@@ -4,7 +4,7 @@ import matplotlib.pylab as plt
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-print(sigmoid(np.array([-1.0, 1.0, 2.0])))
+#  print(sigmoid(np.array([-1.0, 1.0, 2.0])))
 
 #  x = np.arange(-5.0, 5.0, 0.1)
 #  y = sigmoid(x)
